796f7b6ec42190f455d0a99f0a9dc867a419d676
Chinnadurai <81500244+chinna01durai@users.noreply.github.com>
2025-08-26 23:22:48 +0530

    commit

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,7 +2,19 @@ import React from "react";
 import logo from "../assets/image.png";
 
 const header = () => {
-  return <img src={logo} alt="na" />;
+  return (
+    <div className="header">
+      <img className="logo" src={logo} alt="image not found" />
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default header;
