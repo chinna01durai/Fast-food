473abd14d00170d99178b3dfbabbb6df77130740
Chinnadurai <81500244+chinna01durai@users.noreply.github.com>
2025-08-30 21:48:43 +0530

    commit

---
+++ components/Body.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <RestaurantCard />
+    </div>
+  );
+};
+
+export default Body;
---
+++ components/RestaurantCard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const RestaurantCard = () => {
+  return (
+    <div className="res-card">
+      <h3>Meghana Foods</h3>
+    </div>
+  );
+};
+
+export default RestaurantCard;
